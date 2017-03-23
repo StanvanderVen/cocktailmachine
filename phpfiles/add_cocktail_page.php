514334a886b6b6216ff0b2bd19a34ad49afe444f
HesselTjeerdsma <Hesseltjeerdsma@gmail.com>
2017-03-23 20:39:23 +0100

    test page

---
+++ phpfiles/add_cocktail_page.php
@@ -0,0 +1,237 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Cocktail-machine</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
+
+    <!-- Plugin CSS -->
+    <link href="vendor/magnific-popup/magnific-popup.css" rel="stylesheet">
+
+    <!-- Theme CSS -->
+    <link href="css/creative.css" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body id="page-top">
+
+    <nav id="mainNav" class="navbar navbar-default navbar-fixed-top">
+        <div class="container-fluid">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span> Menu <i class="fa fa-bars"></i>
+                </button>
+                <a class="navbar-brand page-scroll" href="#page-top">Cocktail-machine</a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+					<li>
+                        <a class="page-scroll" href="#order">Cocktail bestellen</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href=add_cocktail.php>Cocktail toevoegen</a>
+                    </li>
+					<li>
+						<a class="page-scroll" href="#contact">Contact</a>
+                    </li>
+                    
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container-fluid -->
+    </nav>
+
+	
+    <header>
+        <div class="header-content">
+				<div class="header-content-inner">
+					<div class="container-fluid">	
+						<h1 id="homeHeading">Dynamix</h1>
+						<hr>
+						<p class ="main">Since 1888<p>
+						<a href="#order" class="btn btn-primary btn-xl page-scroll">Bestellen</a>
+					</div>
+				</div>
+		</div>
+    </header>
+
+
+<div class="container-fluid">	
+	<hr>
+	<html>
+	<head>
+		<title>Add cocktail</title>
+		<link rel="stylesheet"type="text/css" href="cocktailmachine.css">
+		
+		
+		<?php 
+		@mysql_connect("localhost", "root", "debollevanplus")
+		or die (mysql_error());
+
+		mysql_select_db("drinks");
+	
+		?>
+			
+	</head> 
+	
+	<body> 
+		<div class="container-fluid">	
+			<div id="left">
+				<div class="col-lg-12 text-center">
+				<form name="form" method="post" action="add_cocktail_post.php"> 
+					<h1 id="homeHeading">Add a cocktail</h1>
+					<div>
+						&nbsp;
+					</div>
+					<div>
+						&nbsp;
+					</div>
+					
+					<div class="form-group row">
+						<label for="example-text-input">Name: </label>
+						<placeholder="Enter Name">
+						<div class="col-10">
+    <input class="form-control" type="text" value="Artisanal kale" id="example-text-input">
+  </div>
+					</div>
+					
+					
+					
+					
+					
+					<ul class="list-group row">
+						<li class="list-group-item">Name: 		</li>
+						<li class="list-group-item">Ingredient 1 </li>
+						<li class="list-group-item">Ingredient 2 </li>
+						<li class="list-group-item">Ingredient 3 </li>
+					</ul>
+					
+					<ul class="list-group row">
+						<li class="list-group-item"><input type="text" size="20" name="name_cocktail"></li>
+						<li class="list-group-item">
+							<select name="ingredient1">
+								<option selected>Choose</option>
+								<?php
+									$query = "SELECT * FROM drinkstable"; //You don't need a ; like you do in SQL
+									$result = mysql_query($query);
+									while($row = mysql_fetch_array($result))
+									{
+										
+										echo "<option>" . $row['Namedrink'] . "</option>";
+									}
+									
+								?>
+								<option>Empty</option>
+								
+							</select>
+							
+							
+						</li>
+						<li class="list-group row">
+							<select name="ingredient2">
+								<option selected>Choose</option>
+								<?php
+									$query = "SELECT * FROM drinkstable"; //You don't need a ; like you do in SQL
+									$result = mysql_query($query);
+									while($row = mysql_fetch_array($result))
+									{
+										
+										echo "<option>" . $row['Namedrink'] . "</option>";
+									}
+									
+								?>
+								<option>Empty</option>
+							</select>
+						</li>
+						<li class="list-group row">
+							<select name="ingredient3">
+								<option selected>Choose</option>
+								<?php
+									$query = "SELECT * FROM drinkstable"; //You don't need a ; like you do in SQL
+									$result = mysql_query($query);
+									while($row = mysql_fetch_array($result))
+									{
+										
+										echo "<option>" . $row['Namedrink'] . "</option>";
+									}
+									
+								?>
+								<option>Empty</option>
+							</select>
+						</li>
+						
+					</ul>
+					
+					<ul class="list-group row">
+						<li class="list-group-item">&nbsp</li>
+						<li class="list-group-item"><input type="text" size="20" name="amount_1"> ml</li>
+						<li class="list-group-item"><input type="text" size="20" name="amount_2"> ml</li>
+						<li class="list-group-item"><input type="text" size="20" name="amount_3"> ml</li>	
+						<li class="list-group-item"><input type="submit" value="Add"></li>
+					</ul>
+					</form> 
+				</div>
+			</div>
+		</div>
+	</body> 
+</html>
+
+</div>
+	
+<footer>
+    <div class="footer-bottom">
+        <div class="container">
+			<a class="page-scroll" href="#contact">Contact</a>
+			&nbsp; 
+            <a class="page-scroll" href="#order"> Cocktail bestellen </a>
+			&nbsp;
+			<a class="page-scroll" href="#contact">Cocktail toevoegen</a> 
+		</div>
+    
+	
+	</footer> <!--/.footerbuttom-->
+
+    <!-- jQuery -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
+    <script src="vendor/scrollreveal/scrollreveal.min.js"></script>
+    <script src="vendor/magnific-popup/jquery.magnific-popup.min.js"></script>
+
+    <!-- Theme JavaScript -->
+    <script src="js/creative.min.js"></script>
+	
+
+
+</body>
+
+</html>
